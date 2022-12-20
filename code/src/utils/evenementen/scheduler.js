2771bc1abda4daaa4c0b6acd3319b2a194376644
ntaheij <noah@taheij.nl>
2022-12-20 13:25:43 +0100

    Added scheduler

--- code/src/utils/evenementen/scheduler.js
+++ code/src/utils/evenementen/scheduler.js
@@ -34,43 +34,15 @@ function checkEvents() {
     let message = `@everyone\n**Alle evenementen van week ${
       new Date().getWeekNumber() + 1
     } (${monday} - ${friday}):**\n\n`;
-    // channel.send(message);
+
+    channel.send(message);
 
     new Promise((resolve, reject) => {
       nextWeek.forEach(async (event, index, array) => {
-        let description = event.description;
-        if (description != null && description.includes(";TAGS;")) {
-          let tmp = description.split(";TAGS; ");
-          description = tmp[0];
-          tags = tmp[1].split(" ");
-          tags = tags.map((tag) => tag.replace("#", ""));
-        }
-
-        let day = new Date(event.start).toLocaleDateString("nl-NL", {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        });
-        let start = new Date(event.start).toLocaleTimeString("nl-NL", {
-          hour: "numeric",
-          minute: "numeric",
-        });
-        let end = new Date(event.end).toLocaleTimeString("nl-NL", {
-          hour: "numeric",
-          minute: "numeric",
-        });
-
-        let date = `${day} van ${start} tot ${end}`;
-
-        message = `**${event.name}**\n${
-          description || ""
-        }\n**Datum:** ${date}\n**Locatie:** ${event.location}\n\n${
-          event.url
-        }\n\n\n`;
+        message = `${event.url}\n`;
         let sentMessage = await channel.send(message);
         data[event.url].announcementMessageId = sentMessage.id;
-        if (index === array.length -1) resolve();
+        if (index === array.length - 1) resolve();
       });
     }).then(() => {
       writeFile("configs", "events", data);
