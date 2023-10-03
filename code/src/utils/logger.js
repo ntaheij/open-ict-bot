009e72c3c905aa7c3716a3d4b235aeaf7f1daa90
ntaheij <noah@taheij.nl>
2023-10-03 13:06:58 +0200

    Events fix

--- code/src/utils/logger.js
+++ code/src/utils/logger.js
@@ -165,53 +165,53 @@ module.exports = c => {
       );
     });
 
-    c.on("guildMemberUpdate", function (oldMember, newMember) {
-      let options = {};
+    // c.on("guildMemberUpdate", function (oldMember, newMember) {
+    //   let options = {};
 
-      if (options[newMember.guild.id]) {
-        options = options[newMember.guild.id];
-      }
+    //   if (options[newMember.guild.id]) {
+    //     options = options[newMember.guild.id];
+    //   }
 
-      // Add default empty list
-      if (typeof options.excludedroles === "undefined")
-        options.excludedroles = new Array([]);
-      if (typeof options.trackroles === "undefined") options.trackroles = true;
-      const oldMemberRoles = oldMember.roles.cache.keyArray();
-      const newMemberRoles = newMember.roles.cache.keyArray();
-      const oldRoles = oldMemberRoles
-        .filter(x => !options.excludedroles.includes(x))
-        .filter(x => !newMemberRoles.includes(x));
-      const newRoles = newMemberRoles
-        .filter(x => !options.excludedroles.includes(x))
-        .filter(x => !oldMemberRoles.includes(x));
-      const rolechanged = newRoles.length || oldRoles.length;
+    //   // Add default empty list
+    //   if (typeof options.excludedroles === "undefined")
+    //     options.excludedroles = new Array([]);
+    //   if (typeof options.trackroles === "undefined") options.trackroles = true;
+    //   const oldMemberRoles = oldMember.roles.cache.keyArray();
+    //   const newMemberRoles = newMember.roles.cache.keyArray();
+    //   const oldRoles = oldMemberRoles
+    //     .filter(x => !options.excludedroles.includes(x))
+    //     .filter(x => !newMemberRoles.includes(x));
+    //   const newRoles = newMemberRoles
+    //     .filter(x => !options.excludedroles.includes(x))
+    //     .filter(x => !oldMemberRoles.includes(x));
+    //   const rolechanged = newRoles.length || oldRoles.length;
 
-      if (rolechanged) {
-        let roleadded = "";
-        if (newRoles.length > 0) {
-          for (let i = 0; i < newRoles.length; i++) {
-            if (i > 0) roleadded += ", ";
-            roleadded += `<@&${newRoles[i]}>`;
-          }
-        }
-        let roleremoved = "";
-        if (oldRoles.length > 0) {
-          for (let i = 0; i < oldRoles.length; i++) {
-            if (i > 0) roleremoved += ", ";
-            roleremoved += `<@&${oldRoles[i]}>`;
-          }
-        }
-        let text = `${roleremoved ? `❌ ROLE REMOVED: \n${roleremoved}` : ""}${roleadded ? `✅ ROLE ADDED:\n${roleadded}` : ""
-          }`;
-        send_log(
-          c,
-          oldMember.guild,
-          `${roleadded ? "GREEN" : "RED"}`,
-          "Member ROLES Changed",
-          `Member: ${newMember.user}\nUser: \`${oldMember.user.tag}\`\n\n${text}`
-        );
-      }
-    });
+    //   if (rolechanged) {
+    //     let roleadded = "";
+    //     if (newRoles.length > 0) {
+    //       for (let i = 0; i < newRoles.length; i++) {
+    //         if (i > 0) roleadded += ", ";
+    //         roleadded += `<@&${newRoles[i]}>`;
+    //       }
+    //     }
+    //     let roleremoved = "";
+    //     if (oldRoles.length > 0) {
+    //       for (let i = 0; i < oldRoles.length; i++) {
+    //         if (i > 0) roleremoved += ", ";
+    //         roleremoved += `<@&${oldRoles[i]}>`;
+    //       }
+    //     }
+    //     let text = `${roleremoved ? `❌ ROLE REMOVED: \n${roleremoved}` : ""}${roleadded ? `✅ ROLE ADDED:\n${roleadded}` : ""
+    //       }`;
+    //     send_log(
+    //       c,
+    //       oldMember.guild,
+    //       `${roleadded ? "GREEN" : "RED"}`,
+    //       "Member ROLES Changed",
+    //       `Member: ${newMember.user}\nUser: \`${oldMember.user.tag}\`\n\n${text}`
+    //     );
+    //   }
+    // });
 
     c.on("messageDelete", function (message) {
       if (message.author.bot) return;
